5a64b8b1012820b5139f310496a11dcd4c2719b3
gonzalo1905 <gonzaloaliaga11@hotmail.com>
2025-06-16 02:27:06 -0500

    feat(Technicians): update AppDbContext.cs

--- OsitoPolarPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ OsitoPolarPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using OsitoPolarPlatform.API.bc_technicians.Domain.Model.Entities;
 using OsitoPolarPlatform.API.ServiceRequests.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using OsitoPolarPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using OsitoPolarPlatform.API.WorkOrders.Infrastructure.Persistence.EFC.Configuration.Extensions;
@@ -8,6 +9,7 @@ namespace OsitoPolarPlatform.API.Shared.Infrastructure.Persistence.EFC.Configura
 
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
+    public DbSet<Technician> Technicians { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
         // Add the created and updated interceptor
@@ -21,6 +23,20 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
         
         // Apply snake_case naming convention for database
         builder.ApplyServiceRequestConfiguration();
+        
+        //technicians
+        builder.Entity<Technician>().HasKey(f => f.Id);
+        builder.Entity<Technician>().Property(f => f.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Technician>().Property(f => f.Name).IsRequired();
+        builder.Entity<Technician>().Property(f => f.CompanyId).IsRequired();
+        builder.Entity<Technician>().Property(f => f.Specialization).IsRequired();
+        builder.Entity<Technician>().Property(f => f.Email).IsRequired();
+        builder.Entity<Technician>().Property(f => f.Phone).IsRequired();
+        builder.Entity<Technician>().Property(f => f.Rating).IsRequired();
+        builder.Entity<Technician>().Property(f => f.Availability).IsRequired();
+
+        
+        
         builder.ApplyWorkOrderConfiguration();
         builder.UseSnakeCaseNamingConvention();
     }
