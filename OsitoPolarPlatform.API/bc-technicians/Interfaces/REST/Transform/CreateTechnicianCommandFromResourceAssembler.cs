035094eeab74b5785680fe01ad83ec3bf1f95805
gonzalo1905 <gonzaloaliaga11@hotmail.com>
2025-06-16 02:26:16 -0500

    feat(Technicians): Add CreateTechnicianCommandFromResourceAssembler

---
+++ OsitoPolarPlatform.API/bc-technicians/Interfaces/REST/Transform/CreateTechnicianCommandFromResourceAssembler.cs
@@ -0,0 +1,21 @@
+using OsitoPolarPlatform.API.bc_technicians.Domain.Model.Commands;
+using OsitoPolarPlatform.API.bc_technicians.Interfaces.REST.Resources;
+
+namespace OsitoPolarPlatform.API.bc_technicians.Interfaces.REST.Transform;
+
+public static class CreateTechnicianCommandFromResourceAssembler
+{
+    public static CreateTechnicianCommand ToCommandFromResource(
+        CreateTechnicianResource resource)
+    {
+        return new CreateTechnicianCommand(
+            resource.Name,
+            resource.Specialization,
+            resource.Phone,
+            resource.Email,
+            resource.Rating,
+            resource.Availability,
+            resource.CompanyId
+        );
+    }
+}
