80262bd09b11eefc2da2d2d3c42f978c634ccc74
eJosR-Coding <joseph.rodriguez.parco@outlook.com>
2025-06-17 02:38:23 -0500

    feat(EquipmentManagement): add assembler for UpdateEquipmentTemperatureCommand

---
+++ OsitoPolarPlatform.API/EquipmentManagement/Interfaces/REST/Transform/UpdateEquipmentTemperatureCommandFromResourceAssembler.cs
@@ -0,0 +1,15 @@
+﻿using OsitoPolarPlatform.API.EquipmentManagement.Domain.Model.Commands;
+using OsitoPolarPlatform.API.EquipmentManagement.Interfaces.REST.Resources;
+
+namespace OsitoPolarPlatform.API.EquipmentManagement.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembles an UpdateEquipmentTemperatureCommand from a resource.
+/// </summary>
+public static class UpdateEquipmentTemperatureCommandFromResourceAssembler
+{
+    public static UpdateEquipmentTemperatureCommand ToCommandFromResource(int equipmentId, UpdateEquipmentTemperatureResource resource)
+    {
+        return new UpdateEquipmentTemperatureCommand(equipmentId, resource.Temperature);
+    }
+}
