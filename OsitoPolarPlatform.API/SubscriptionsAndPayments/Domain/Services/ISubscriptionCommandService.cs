976bd17cf5b1d186aea2bf8d477e172ff9f2a23b
eJosR-Coding <joseph.rodriguez.parco@outlook.com>
2025-07-10 18:58:33 -0500

    feat(ISubscriptionCommandService): add handlers for creating and deleting subscriptions

--- OsitoPolarPlatform.API/SubscriptionsAndPayments/Domain/Services/ISubscriptionCommandService.cs
+++ OsitoPolarPlatform.API/SubscriptionsAndPayments/Domain/Services/ISubscriptionCommandService.cs
@@ -6,4 +6,6 @@ namespace OsitoPolarPlatform.API.SubscriptionsAndPayments.Domain.Services;
 public interface ISubscriptionCommandService
 {
     Task<Subscription?> Handle(UpgradePlanCommand command);
+    Task<Subscription?> Handle(CreateSubscriptionCommand command);
+    Task<bool> Handle(DeleteSubscriptionCommand command);
 }
