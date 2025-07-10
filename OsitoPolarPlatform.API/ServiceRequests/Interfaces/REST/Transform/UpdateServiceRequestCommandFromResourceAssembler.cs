a0866e155c4515a5488f4e1e64563003d898a695
eJosR-Coding <joseph.rodriguez.parco@outlook.com>
2025-07-10 18:59:17 -0500

    fix(UpdateServiceRequestCommandFromResourceAssembler): simplify status, priority, urgency, and service type handling in command conversion

--- OsitoPolarPlatform.API/ServiceRequests/Interfaces/REST/Transform/UpdateServiceRequestCommandFromResourceAssembler.cs
+++ OsitoPolarPlatform.API/ServiceRequests/Interfaces/REST/Transform/UpdateServiceRequestCommandFromResourceAssembler.cs
@@ -1,29 +1,22 @@
 using OsitoPolarPlatform.API.ServiceRequests.Domain.Model.Commands;
-using OsitoPolarPlatform.API.ServiceRequests.Domain.Model.ValueObjects;
 using OsitoPolarPlatform.API.ServiceRequests.Interfaces.REST.Resources;
 
 namespace OsitoPolarPlatform.API.ServiceRequests.Interfaces.REST.Transform;
-
 
 public static class UpdateServiceRequestCommandFromResourceAssembler
 {
     public static UpdateServiceRequestCommand ToCommandFromResource(int id, UpdateServiceRequestResource resource)
     {
-        Enum.TryParse(resource.Status, true, out EServiceRequestStatus status);
-        Enum.TryParse(resource.Priority, true, out EPriority priority);
-        Enum.TryParse(resource.Urgency, true, out EUrgency urgency);
-        Enum.TryParse(resource.ServiceType, true, out EServiceType serviceType);
-
         return new UpdateServiceRequestCommand(
             id,
             resource.Title,
             resource.Description,
             resource.IssueDetails,
-            status,
-            priority,
-            urgency,
+            resource.Status,       
+            resource.Priority,     
+            resource.Urgency,     
             resource.IsEmergency,
-            serviceType, 
+            resource.ServiceType,  
             resource.AssignedTechnicianId,
             resource.ScheduledDate,
             resource.TimeSlot,
