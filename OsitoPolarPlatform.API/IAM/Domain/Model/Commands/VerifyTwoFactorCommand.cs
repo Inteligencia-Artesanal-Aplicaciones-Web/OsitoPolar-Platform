51b990bcfceb03b6641c411d7723b9bc428eb448
eJosR-Coding <joseph.rodriguez.parco@outlook.com>
2025-11-05 23:38:11 -0500

    feat: add VerifyTwoFactorCommand for two-factor authentication verification

---
+++ OsitoPolarPlatform.API/IAM/Domain/Model/Commands/VerifyTwoFactorCommand.cs
@@ -0,0 +1,8 @@
+namespace OsitoPolarPlatform.API.IAM.Domain.Model.Commands;
+
+/// <summary>
+/// Command to verify a two-factor authentication code
+/// </summary>
+/// <param name="Username">The username of the user</param>
+/// <param name="Code">The 6-digit TOTP code from Google Authenticator</param>
+public record VerifyTwoFactorCommand(string Username, string Code);
